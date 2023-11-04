529dfb060b75824bfe8b898752f9ed92c9b68d4c
Priyanshisri23 <149654773+Priyanshisri23@users.noreply.github.com>
2023-11-04 11:55:12 +0530

    Update and rename FileReadInventorymanagement.py to Master_File_Inventory_Management.py

--- Code Inventory Management/Master_File_Inventory_Management.py
+++ Code Inventory Management/Master_File_Inventory_Management.py
@@ -1,31 +1,33 @@
 import pandas as pd
 import datetime
 import logging
-import InventoryManagementConfig
-import FileFolderInventory
+import traceback
+import Config_File_Inventory_Managment
+import File_Folder_Inventory_Managment
 
 print("Start Time",datetime.datetime.now())
 current_date = datetime.date.today()
 Log_date=current_date.strftime("%d%B%Y")
 
 try:
-    log_file_path = fr"{FileFolderInventory.LogFolder}\ProcessLog_{Log_date}.log"
+    log_file_path = fr"{File_Folder_Inventory_Managment.LogFolder}\ProcessLog_{Log_date}.log"
     logging.basicConfig(filename=log_file_path, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info("Master File Process has been Started")
 
-    MB52Dumpdf=pd.read_excel(FileFolderInventory.MB52FilePath,header=1)
-    mch1df=pd.read_excel(FileFolderInventory.mch1_v2_dateFilePath)
-    mchadf=pd.read_excel(FileFolderInventory.MCHAFilePath)
-    ZFI_ClosingStockdf=pd.read_excel(FileFolderInventory.ZFI_ClosingStockFilePath,header=1)
-    PriceListdf=pd.read_excel(FileFolderInventory.PriceListFilePath,header=1)
-    InHouseBhismaNeedledf=pd.read_excel(FileFolderInventory.BhismaFilePath,header=1,sheet_name=InventoryManagementConfig.InHouseBhismaNeedleSheet)
-    QNPLBhismaNeedledf=pd.read_excel(FileFolderInventory.BhismaFilePath,header=1,sheet_name=InventoryManagementConfig.QNPLBhismaNeedleSheet)
-    SutureBhismaNeedledf=pd.read_excel(FileFolderInventory.BhismaFilePath,header=1,sheet_name=InventoryManagementConfig.SutureBhismaNeedleSheet)
-    DivisionSummarydf=pd.read_excel(FileFolderInventory.DivisionSummaryFilePath,header=2)
-    SLOC_nonproductivelocationdf=pd.read_excel(FileFolderInventory.SLOC_nonproductivelocationFilePath)
-    lastQuarterInventorydf=pd.read_excel(FileFolderInventory.LastQuarterInventoryFilePath)
-    ageing_masterdf = pd.read_excel(FileFolderInventory.AgeingMasterFilePath,header=1)
+    MB52Dumpdf = pd.read_excel(File_Folder_Inventory_Managment.MB52FilePath,header=1)
+    mch1df = pd.read_excel(File_Folder_Inventory_Managment.mch1_v2_dateFilePath)
+    mchadf = pd.read_excel(File_Folder_Inventory_Managment.MCHAFilePath)
+    ZFI_ClosingStockdf = pd.read_excel(File_Folder_Inventory_Managment.ZFI_ClosingStockFilePath,header=1)
+    PriceListdf = pd.read_excel(File_Folder_Inventory_Managment.PriceListFilePath,header=1)
+    InHouseBhismaNeedledf = pd.read_excel(File_Folder_Inventory_Managment.BhismaFilePath,header=1,sheet_name=Config_File_Inventory_Managment.InHouseBhismaNeedleSheet)
+    QNPLBhismaNeedledf = pd.read_excel(File_Folder_Inventory_Managment.BhismaFilePath,header=1,sheet_name=Config_File_Inventory_Managment.QNPLBhismaNeedleSheet)
+    SutureBhismaNeedledf = pd.read_excel(File_Folder_Inventory_Managment.BhismaFilePath,header=1,sheet_name=Config_File_Inventory_Managment.SutureBhismaNeedleSheet)
+    DivisionSummarydf = pd.read_excel(File_Folder_Inventory_Managment.DivisionSummaryFilePath,header=2)
+    SLOC_nonproductivelocationdf = pd.read_excel(File_Folder_Inventory_Managment.SLOC_nonproductivelocationFilePath)
+    lastQuarterInventorydf = pd.read_excel(File_Folder_Inventory_Managment.LastQuarterInventoryFilePath)
+    ageing_masterdf = pd.read_excel(File_Folder_Inventory_Managment.AgeingMasterFilePath,header=1)
 
-    logging.info('Prepared all input files  dataframe')
+    logging.info('Prepared all input files into dataframe')
 
     # Column V (Total Stock Quantity) = Sum of column J to O 
     MB52Dumpdf['Total Stock Quantity'] = MB52Dumpdf['Unrestricted'] + MB52Dumpdf['Transit and Transfer'] + MB52Dumpdf['Quality Inspection']+ MB52Dumpdf['Restricted-Use Stock']+ MB52Dumpdf['Blocked']+ MB52Dumpdf['Returns']
@@ -46,6 +48,7 @@ try:
     MB52Dumpdf.insert(29, 'Plant Sku & Val Type','')
     MB52Dumpdf.insert(30, 'Mat dec & valn type','')
     MB52Dumpdf.insert(31, 'SKU & Valn Type','')
+    logging.info("Merged the Columns and Created New Column")
 
     # ==================================Valuation Type=======================================
     # mcha  file   to get the “Valuation Type” column from MCHA file 
@@ -57,19 +60,20 @@ try:
     MB52Dumpdf['Mat dec & valn type'] = MB52Dumpdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Material description'], row['Valan type from MCHA(SKU+Batch)']]), axis=1)
     MB52Dumpdf['SKU & Valn Type'] = MB52Dumpdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Material'], row['Valan type from MCHA(SKU+Batch)']]), axis=1)
     
-    
-    
+
     # =====================ZFI Closing Stock================
     ZFI_ClosingStockdf['Concatenate(Plant+SKU+Batch)'] = ZFI_ClosingStockdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Plant'], row['Material'], row['Batch']]), axis=1)
     ZFI_ClosingStockdf['Concatenate(SKU+Batch)'] = ZFI_ClosingStockdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Material'], row['Batch']]), axis=1)
     ZFI_ClosingStockdf['Concatenate(SKU+Valuation type)'] = ZFI_ClosingStockdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Material'], row['Valuation Type']]), axis=1)
     ZFI_ClosingStockdf['Concatenate(Mat desc+Valuation type)'] = ZFI_ClosingStockdf.apply(lambda row: ''.join(str(col) if pd.notnull(col) else '' for col in [row['Material description'], row['Valuation Type']]), axis=1)
 
-    output_file_path = rf"{InventoryManagementConfig.OutputFolder}\MB52OutputInventorymanagement.xlsx"
+    output_file_path = rf"{Config_File_Inventory_Managment.OutputFolder}\MB52OutputInventorymanagement.xlsx"
     with pd.ExcelWriter(output_file_path, engine='openpyxl', mode='w') as writer:
         MB52Dumpdf.to_excel(writer,sheet_name='MB52', index=False)
 
     print("End Time",datetime.datetime.now())
-    
+    logging.info(f"Master File Process has Ended on: {datetime.datetime.now()}")
 except Exception as e:
-   logging.info(e)
+    ErrorMessage = traceback.extract_tb(e.__traceback__)
+    line_number = ErrorMessage[-1][1]
+    logging.error(f"Error in File & Folder message: {ErrorMessage} in line {line_number}")
