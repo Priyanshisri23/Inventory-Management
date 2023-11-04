2af93f4ff80c8dad3009bb5a2e39aa2f5638df00
Priyanshisri23 <149654773+Priyanshisri23@users.noreply.github.com>
2023-11-04 11:47:49 +0530

    Config_File_Inventory_Managment.py

--- Code Inventory Management/InventoryManagementConfig.py
+++ Code Inventory Management/InventoryManagementConfig.py
@@ -3,6 +3,7 @@ import json
 import os
 import logging
 import datetime
+import traceback
 
 current_date = datetime.date.today()
 Log_date=current_date.strftime("%d%B%Y")
@@ -11,43 +12,48 @@ directory_path = os.path.dirname(full_path)
 log_file_path = fr"{directory_path}\ProcessLog_{Log_date}.log"
 logging.basicConfig(filename=log_file_path, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 try:
+    logging.info(f"The Process has been Started...")
+    logging.info(f"Searching for Config File on the Path")
     ConfigFilePath=r"D:\All The Task\Python Project\Inventory Management\Config\ConfigFile_Inventory_Management.csv"
     if not os.path.exists(ConfigFilePath):
         ConfigFilePath=r"D:\All The Task\Python Project\Inventory Management\Template\ConfigFile_Inventory_Management.csv"
     logging.info("Config file exist in the folder")
-except Exception as e:
-    logging.error(e)
-try:
-    df = pd.read_csv(ConfigFilePath, header=0, index_col=0)
-    c = {index: value for index, value in df.iloc[:, 0].items()}
-    json_c = json.dumps(c)
-    data = json.loads(json_c)
-    MainFolder = data['MainFolder']
-    ConfigFolder = data['ConfigFolder']
-    InputFolder= data['InputFolder']
-    OutputFolder= data['OutputFolder']
-    LogFolder= data['LogFolder']
-    TemplateFolder=data['TemplateFolder']
-    Bhismafile= data['Bhismafile']
-    LastQuarterInventoryFile= data['LastQuarterInventoryFile']
-    MCHAFile= data['MCHAFile']
-    mch1_v2_datefile= data['mch1_v2_datefile']
-    InputFolder= data['InputFolder']
-    PriceListfile=data['PriceListfile']
-    DivisionSummaryFile=data['DivisionSummaryFile']
-    AgeingMasterFile=data['AgeingMasterFile']
-    MB52File=data['MB52File']
-    ZFI_ClosingStockFile=data['ZFI_ClosingStockFile']
-    SLOC_nonproductivelocationFile=data['SLOC_nonproductivelocationFile']
-    InHouseBhismaNeedleSheet=data['InHouseBhismaNeedleSheet']
-    QNPLBhismaNeedleSheet= data['QNPLBhismaNeedleSheet']
-    SutureBhismaNeedleSheet = data['SutureBhismaNeedleSheet']
-    EmailTo= data['EmailTo']
-    EmailCC= data['EmailCC']
-    EmailFrom= data['EmailFrom']
-    EmailPassword = data['EmailPassword']
-    logging.info("Read all details from Config File")
-except Exception as e:
-    logging.error(e)
-
     
+    try:
+        logging.info(f"Fetching Details from Config File...")
+        df = pd.read_csv(ConfigFilePath, header=0, index_col=0)
+        DataIntoDict = {index: value for index, value in df.iloc[:, 0].items()}
+        Get_JSON_Data = json.dumps(DataIntoDict)
+        Read_JSON_Data = json.loads(Get_JSON_Data)
+        MainFolder = Read_JSON_Data['MainFolder']
+        ConfigFolder = Read_JSON_Data['ConfigFolder']
+        InputFolder = Read_JSON_Data['InputFolder']
+        OutputFolder = Read_JSON_Data['OutputFolder']
+        LogFolder = Read_JSON_Data['LogFolder']
+        TemplateFolder = Read_JSON_Data['TemplateFolder']
+        Bhismafile = Read_JSON_Data['Bhismafile']
+        LastQuarterInventoryFile = Read_JSON_Data['LastQuarterInventoryFile']
+        MCHAFile = Read_JSON_Data['MCHAFile']
+        mch1_v2_datefile = Read_JSON_Data['mch1_v2_datefile']
+        PriceListfile = Read_JSON_Data['PriceListfile']
+        DivisionSummaryFile = Read_JSON_Data['DivisionSummaryFile']
+        AgeingMasterFile = Read_JSON_Data['AgeingMasterFile']
+        MB52File = Read_JSON_Data['MB52File']
+        ZFI_ClosingStockFile = Read_JSON_Data['ZFI_ClosingStockFile']
+        SLOC_nonproductivelocationFile = Read_JSON_Data['SLOC_nonproductivelocationFile']
+        InHouseBhismaNeedleSheet = Read_JSON_Data['InHouseBhismaNeedleSheet']
+        QNPLBhismaNeedleSheet = Read_JSON_Data['QNPLBhismaNeedleSheet']
+        SutureBhismaNeedleSheet = Read_JSON_Data['SutureBhismaNeedleSheet']
+        EmailTo = Read_JSON_Data['EmailTo']
+        EmailCC = Read_JSON_Data['EmailCC']
+        EmailFrom = Read_JSON_Data['EmailFrom']
+        EmailPassword = Read_JSON_Data['EmailPassword']
+        logging.info("Read all details from Config File")
+    except Exception as e:
+        ErrorMessage = traceback.extract_tb(e.__traceback__)
+        line_number = ErrorMessage[-1][1]
+        logging.warning(f"Warning in Config File message: {ErrorMessage} in line {line_number}")
+except Exception as e:
+    ErrorMessage = traceback.extract_tb(e.__traceback__)
+    line_number = ErrorMessage[-1][1]
+    logging.error(f"Error in Config File message: {ErrorMessage} in line {line_number}")
